1b88c59de2c52da3367223629cbcbb59577c95c1
Ritesh Naik <riteshnaik55555@gmail.com>
2020-06-15 23:51:42 +0530

    complete done without back button handling

--- customize.py
+++ customize.py
@@ -21,14 +21,16 @@ class customizePage(BoxLayout):
         self.deviceid = did
         self.modf = modif
         f = open(f"devices\{self.deviceid}.json")
+
+        #unmodified json data
         self.data = json.load(f)
-        #self.newData = self.data.copy()
+
+        #new dictionary to store modified values
         self.newData = {"mapType" : {"capability":[], "resourceType":[],"resourceHref":[]} , "mapData" : {}}
         
+        #layout design 
         self.orientation = "vertical"
 
-        #data required to display
-        
         #title box
         content = BoxLayout(orientation = 'horizontal', size_hint_y = .2  )
         content.add_widget(Label(text='OCF Resource Tool' , font_size = 30))
@@ -39,35 +41,62 @@ class customizePage(BoxLayout):
         content.add_widget(Label(text='Customize Selected Resource type for ' + self.deviceid , font_size = 20))
         self.add_widget(content)
 
-        #scrollbox
+        #scrollbox for customizing items
         scroll = ScrollView()
         content = GridLayout(cols = 1 , padding = (10,10) , spacing = (10,10) ,size_hint_y = None  , row_default_height = 35)
         content.bind(minimum_height=content.setter('height'))
+
+        #dictionary to store updated command
+        self.updatedCommand = {}
+        #dictionary for checkbox to check if command is selected
+        self.checkbox = {}
+
+        #only include resources that were previuosly selected
         for i in range(len(self.data['mapType']['resourceType'])):
             string1 = self.data['mapType']['resourceType'][i][0]
             string2 = self.data['mapType']['capability'][i][0]
             if self.modf[i][0] == True :
+
+                #store updated values in new dictionary
                 self.newData["mapType"]["capability"].append(self.data["mapType"]["capability"][i])
                 self.newData["mapType"]["resourceType"].append(self.data["mapType"]["resourceType"][i])
                 self.newData["mapType"]["resourceHref"].append(self.modf[i][1])
                 self.newData["mapData"][string2] =  self.data["mapData"][string2]
             
                 if "valueMap" in self.data['mapData'][string2] :
+                    #creating layout for commands that need to be customized
+
                     minContent = BoxLayout(orientation="horizontal")
                     minContent.add_widget(Label(text = string1 + self.modf[i][1] , size_hint_x = .5 ))
                     content.add_widget(minContent)
+                    
+                    #add the capability in updatedCommand and checkbox
+                    self.updatedCommand[string2] = []
+                    self.checkbox[string2] = []
+                    
+                    #data of map Values
                     arr1 = self.data['mapData'][string2]["valueMap"][0]["STValue"]
                     arr2 = self.data['mapData'][string2]["valueMap"][0]["OCFValue"]
                     for j in range(len(arr1)):
+                        #layout for each command
                         minContent = BoxLayout(orientation="horizontal")
-                        minContent.add_widget(CheckBox())
+
+                        #adding checkbox
+                        self.checkbox[string2].append( CheckBox() )
+                        minContent.add_widget(self.checkbox[string2][j])
+                        
+                        #adding required mapping labels
                         minContent.add_widget(Label(text=arr1[j]))
                         minContent.add_widget(Label(text = "OIC Resource Mapping value"))
-                        minContent.add_widget(TextInput(text = str(arr2[j])))
+                        
+                        #adding boxes
+                        self.updatedCommand[string2].append(TextInput(text = str(arr2[j])))
+                        minContent.add_widget(self.updatedCommand[string2][j])
                         content.add_widget(minContent)
 
         scroll.add_widget(content)
         self.add_widget(scroll)
+
         
         #Generate button
         content  = FloatLayout(size_hint_y = .2)
@@ -79,25 +108,46 @@ class customizePage(BoxLayout):
         content.add_widget(generate)
         self.add_widget(content)
 
+    #function to go to previous page
     def goBack(self):
         App.get_running_app().screenManager.transition.direction = 'right' 
         App.get_running_app().screenManager.current = 'device'
 
+
+
+    #function to open a popup that has option to download modified file
     def generateJSON(self):
-        print (json.dumps(self.newData, sort_keys=True, indent=4))
+        
+        ##### update the self.newData with customized command   #####
+        for i in range(len(self.data['mapType']['resourceType'])):
+            capability = self.data['mapType']['capability'][i][0]
+            if (self.modf[i][0] == True) and ("valueMap" in self.data['mapData'][capability]):
+                for j in range(len(self.checkbox[capability])) :
+                    if self.checkbox[capability][j].active == True:
+                        self.newData['mapData'][capability]["valueMap"][0]["OCFValue"][j] = self.updatedCommand[capability][j].text
+        ###############################################################
+        print (json.dumps(self.newData,sort_keys=True, indent=2))
+        # adding popup
         popup = Popup(title='JSON file',size_hint=(.9, .9))
         popup.content = BoxLayout(orientation="vertical")
         popup.content.add_widget(Label(text="Click Download button to save the updated JSON file in the current folder"))
+        
+        #back button
         layout = FloatLayout(size_hint_y = .2)
         button = Button(text=' Close ', size_hint = (.2,.4) , pos_hint = {'x':.3 , 'y' : .4} )
         button.bind(on_press=popup.dismiss)
         layout.add_widget(button)
+        
+        #download button
         downld = Button(text = "Download" , size_hint = (.2,.4) , pos_hint = {'x':.5 , 'y' : .4})
         downld.bind(on_press = lambda x:self.downloadFile())
         layout.add_widget(downld)
         popup.content.add_widget(layout)
         popup.open()
 
+
+
+    #function to download the file in the same folder
     def downloadFile(self):
-        with open('newData.json', 'w') as fp:
+        with open(f'new_{self.deviceid}.json', 'w') as fp:
             json.dump(self.newData, fp,  indent=2)
